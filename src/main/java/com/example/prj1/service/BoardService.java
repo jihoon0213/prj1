dbf5a466bd6a0439b1e4e03555e545c2f936333f
jihoon0213 <jihun8138@gmail.com>
2025-06-20 10:50:27 +0900

    글 목록

--- src/main/java/com/example/prj1/service/BoardService.java
+++ src/main/java/com/example/prj1/service/BoardService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,5 +25,12 @@ public class BoardService {
         boardRepository.save(board);
 
 
+    }
+
+    public List<Board> list() {
+        List<Board> list = boardRepository.findAll();
+
+
+        return list;
     }
 }
