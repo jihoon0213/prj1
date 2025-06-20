83e3c85a74bc72965f88a8586c4a5211b99821df
jihoon0213 <jihun8138@gmail.com>
2025-06-20 10:17:03 +0900

    controller, form 입력 dto

--- src/main/java/com/example/prj1/controller/BoardController.java
+++ src/main/java/com/example/prj1/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.example.prj1.controller;
 
+import com.example.prj1.dto.BoardForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,13 @@ public class BoardController {
 
     @GetMapping("write")
     public String write() {
+
+        return "board/write";
+    }
+
+    @PostMapping("write")
+    public String writePost(BoardForm data) {
+        System.out.println("data = " + data);
 
         return "board/write";
     }
