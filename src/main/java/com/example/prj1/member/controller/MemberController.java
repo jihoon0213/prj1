04fdb2200a8c912908eea50a1193e82704c9bbac
jihoon0213 <jihun8138@gmail.com>
2025-06-24 15:28:18 +0900

    탈퇴 후 로그아웃

--- src/main/java/com/example/prj1/member/controller/MemberController.java
+++ src/main/java/com/example/prj1/member/controller/MemberController.java
@@ -80,14 +80,16 @@ public class MemberController {
     public String remove(MemberForm data,
                          @SessionAttribute(value = "loggedInUser", required = false)
                          MemberDto user,
+                         HttpSession session,
                          RedirectAttributes rttr) {
-        // TODO : 작성한 글이 있으면 탈퇴 안됨
+
         boolean result = memberService.remove(data, user);
 
         if (result) {
             rttr.addFlashAttribute("alert",
                     Map.of("code", "danger", "message", data.getId() + "님 탈퇴 되었습니다."));
 
+            session.invalidate();
             return "redirect:/board/list";
         } else {
             rttr.addFlashAttribute("alert",
